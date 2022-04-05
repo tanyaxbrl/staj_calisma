b035fbee8e867e209ace672fbb78062a04cc6876
nursenakocaturk <nursenakocaturk26@gmail.com>
2022-04-05 16:11:43 +0300

    fasterq doğru çalışıyor

--- scripts/rnaseq.sh
+++ scripts/rnaseq.sh
@@ -2,8 +2,8 @@
 
 SRR=SRR18156027
 
-~/Software/sratoolkit.3.0.0-ubuntu64/bin/fasterq-dump ${SRR} \ 
-    --progress \ 
-    --skip-technical \ 
-    --split-files \ 
+~/Software/sratoolkit.3.0.0-ubuntu64/bin/fasterq-dump ${SRR} \
+    --progress \
+    --skip-technical \
+    --split-files \
     --outdir data/raw/
