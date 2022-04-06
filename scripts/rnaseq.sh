46a756b1ffa31304948ed0fc9dafa16ec03fa869
nursenakocaturk <nursenakocaturk26@gmail.com>
2022-04-07 00:23:59 +0300

    parametre değiştirildi

--- scripts/rnaseq.sh
+++ scripts/rnaseq.sh
@@ -5,5 +5,5 @@ SRR=SRR18156027
 ~/Software/sratoolkit.3.0.0-ubuntu64/bin/fasterq-dump ${SRR} \
     --progress \
     --skip-technical \
-    --split-files \
-    --outdir data/raw/
+    --split-spot \
+    --outdir data/raw/ 
