62adedcd5c5c351e1ad3553033c54c0d1f115dca
emrahkirdok <emrahkirdok@gmail.com>
2022-04-12 16:55:59 +0300

    cutadapt eklendi

--- scripts/rnaseq.sh
+++ scripts/rnaseq.sh
@@ -2,6 +2,13 @@
 
 SRR=ERR3473047
 
+ADAPTER1=AGATCGGAAGAG
+ADAPTER2=AGATCGGAAGAG
+THREADS=4
+Q1=20
+Q2=20
+MIN_LEN=10
+
 fasterq-dump ${SRR} \
     --progress \
     --skip-technical \
@@ -10,3 +17,16 @@ fasterq-dump ${SRR} \
 
 fastqc data/raw/${SRR}_1.fastq data/raw/${SRR}_2.fastq
 
+mkdir -p data/processed
+
+conda activate cutadapt
+
+cutadapt -q ${Q1} -Q ${Q2} \
+	-m ${MIN_LEN} --trim-n \
+	-Z -j ${THREADS} \
+	-a ${ADAPTER1} -A ${ADAPTER2} \
+	-o data/processed/${SRR}_1.fastq.gz \
+	-p data/processed/${SRR}_2.fastq.gz \
+	data/raw/${SRR}_1.fastq data/raw/${SRR}_2.fastq
+
+fastqc data/processed/${SRR}_1.fastq.gz data/processed/${SRR}_2.fastq.gz
