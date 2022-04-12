b02830fb30e964458eb98a6faa050e9c92967daf
nursenakocaturk <nursenakocaturk26@gmail.com>
2022-04-12 22:50:35 +0300

    cutadapt çıkartıldı

--- scripts/rnaseq.sh
+++ scripts/rnaseq.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-# kullanmak icin once conda activate cutadapt yaz
-# ./scripts/rnaseq.sh
 
 SRR=ERR3473047
-
-ADAPTER1=AGATCGGAAGAG
-ADAPTER2=AGATCGGAAGAG
-THREADS=4
-Q1=20
-Q2=20
-MIN_LEN=10
 
 fasterq-dump ${SRR} \
     --progress \
@@ -19,16 +10,3 @@ fasterq-dump ${SRR} \
 
 fastqc data/raw/${SRR}_1.fastq data/raw/${SRR}_2.fastq
 
-mkdir -p data/processed
-
-#conda activate cutadapt
-
-cutadapt -q ${Q1} -Q ${Q2} \
-	-m ${MIN_LEN} --trim-n \
-	-Z -j ${THREADS} \
-	-a ${ADAPTER1} -A ${ADAPTER2} \
-	-o data/processed/${SRR}_1.fastq.gz \
-	-p data/processed/${SRR}_2.fastq.gz \
-	data/raw/${SRR}_1.fastq data/raw/${SRR}_2.fastq
-
-fastqc data/processed/${SRR}_1.fastq.gz data/processed/${SRR}_2.fastq.gz
