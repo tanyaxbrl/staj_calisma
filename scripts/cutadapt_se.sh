3ec7163801bc726b37ae14e344439e9057086027
nursenakocaturk <nursenakocaturk26@gmail.com>
2022-04-21 20:40:54 +0300

    dosya oluşturuldu, single end cutadapt kodu eklend

---
+++ scripts/cutadapt_se.sh
@@ -0,0 +1,19 @@
+
+SRR=SRR649944
+ADAPTER1=AGATCGGAAGAG
+ADAPTER2=AGATCGGAAGAG
+THREADS=4
+Q1=20
+Q2=20
+MIN_LEN=10
+
+mkdir -p data/processed
+
+#conda activate cutadapt
+
+cutadapt -q ${Q1} -Q ${Q2} \
+	-m ${MIN_LEN} --trim-n \
+	-Z -j ${THREADS} \
+	-a ${ADAPTER1} -A ${ADAPTER2} \
+	-o data/processed/${SRR}.fastq.gz \
+	data/raw/${SRR}.fastq
