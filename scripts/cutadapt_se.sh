332d414691b98a49aee737a84b6a7741a8ffdfb3
nursenakocaturk <nursenakocaturk26@gmail.com>
2022-04-22 10:26:51 +0300

    düzeltmeler yapıldı

--- scripts/cutadapt_se.sh
+++ scripts/cutadapt_se.sh
@@ -5,14 +5,13 @@ ADAPTER1=AGATCGGAAGAG
 ADAPTER2=AGATCGGAAGAG
 THREADS=4
 Q1=20
-Q2=20
 MIN_LEN=10
 
 mkdir -p data/processed
 
 #conda activate cutadapt
 
-cutadapt -q ${Q1} -Q ${Q2} \
+cutadapt -q ${Q1} \
 	-m ${MIN_LEN} --trim-n \
 	-Z -j ${THREADS} \
 	-a ${ADAPTER1} -A ${ADAPTER2} \
