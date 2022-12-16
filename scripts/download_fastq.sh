600d23a459888d179f14b27433001b00f86619f7
nursenakocaturk <nursenakocaturk26@gmail.com>
2022-12-16 11:43:43 +0300

    download kopyalandi

--- scripts/download_fastq.sh
+++ scripts/download_fastq.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 SRR=$1
+END=$2
 
-SRA_OUT=data/SRA
+SRA_OUT=data/sra
+FASTQ_OUT=data/raw/${END}
 
 prefetch ${SRR} -O ${SRA_OUT}
 
-fasterq-dump \
-	--progress \
-	--split-3 \
-	${SRA_OUT}/${SRR} \
-	-O data/raw/ 
-
-#fastqc data/raw/${SRR}_1.fastq data/raw/${SRR}_2.fastq
+fasterq-dump ${SRA_OUT}/${SRR} -O ${FASTQ_OUT}
 
