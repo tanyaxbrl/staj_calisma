efc43afae51b8354789226ff9bfd033e723392f3
nursenakocaturk <nursenakocaturk26@gmail.com>
2022-12-16 11:49:01 +0300

    bwa kopyalandi

---
+++ scripts/bwa_pe.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# kullanmak icin conda activate cutadapt
+
+mkdir -p results
+
+SRR=$1
+END=$2
+FASTA=$3
+
+RAW_OUT=data/raw
+REF_FOLDER=data/ref/
+
+
+
+bwa aln -t 4 \
+	${REF_FOLDER}/${FASTA} \
+	data/processed/${END}/${SRR}_1.fastq.gz > results/aligment/${END}/${SRR}_1_p.sai
+	
+bwa aln -t 4 \
+	${REF_FOLDER}/${FASTA} \
+	data/processed/${END}/${SRR}_2.fastq.gz > results/aligment/${END}/${SRR}_2_p.sai
+
+bwa sampe ${REF_FOLDER}/${FASTA} \
+	results/${SRR}_1_p.sai \
+	results/${SRR}_2_p.sai \
+	data/processed/${END}/${SRR}_1.fastq.gz \
+	data/processed/${END}/${SRR}_2.fastq.gz > results/aligment/${END}/${SRR}.sam
--- scripts/bwa_se.sh
+++ scripts/bwa_se.sh
@@ -2,22 +2,19 @@
 # kullanmak icin conda activate cutadapt
 
 mkdir -p results
-
+mkdir -p results/aligment
 SRR=$1
+END=$2
+FASTA=$3
 
 RAW_OUT=data/raw
 REF_FOLDER=data/ref/
 
-PROCESSED_OUT=data/processed
-
-FASTA=GCF_000007565.2_ASM756v2_genomic.fna
-
-bwa index data/ref/${FASTA}
 
 bwa aln -t 4 \
 	${REF_FOLDER}/${FASTA} \
-	${PROCESSED_OUT}/${SRR}.fastq > results/${SRR}_p.sai
+	data/processed/${END}/${SRR}.fastq.gz > results/aligment/${END}/${SRR}_p.sai
 	
 bwa samse ${REF_FOLDER}/${FASTA} \
 	results/${SRR}_p.sai \
-	${PROCESSED_OUT}/${SRR}.fastq > results/${SRR}.sam
+	data/processed/${END}/${SRR}.fastq.gz > results/aligment/${END}/${SRR}.sam
