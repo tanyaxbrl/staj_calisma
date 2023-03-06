8e9fc84cc0f5443f95394918804ce278c0e4749e
nursenakocaturk <nursenakocaturk26@gmail.com>
2023-03-06 15:51:37 +0300

    part4.sh betik dosyası oluşturuldu

---
+++ part4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir -p results/star/pe
+mkdir -p results/star/se
+
+REFERENCE=GCF_000412675.1_ASM41267v1_genomic.fna
+THREADS=8
+
+STAR --runMode genomeGenerate \
+    --genomeDir data/ref/GenomeDir \
+    --genomeFastaFiles ${REFERENCE} \
+    --runThreadN ${THREADS}
+        
+while read LINE
+do 
+        SRR=$(echo "$LINE" | cut -d " " -f1)
+        END=$(echo "$LINE" | cut -d " " -f2)
+
+        if [ ${END} == "pe" ]
+        then 
+            ./scripts/star_pe.sh ${SRR} ${END} ${REFERENCE}
+        else 
+            ./scripts/star_se.sh ${SRR} ${END} ${REFERENCE}
+        fi
+
+done < data.txt
+
