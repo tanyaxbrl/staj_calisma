c8048eec9f44c5d6a600add1081b444d765d55ac
nursenakocaturk <nursenakocaturk26@gmail.com>
2023-03-06 15:43:29 +0300

    star_pe.sh betik dosyası oluşturuldu

---
+++ scripts/star_pe.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# kullanmak icin conda activate rnaseq
+
+mkdir -p results/star
+
+SRR=$1
+END=$2
+FASTA=$3
+THREADS=8
+
+REF_FOLDER=data/ref/
+
+STAR --runThreadN ${THREADS} \
+	--genomeDir ${REF_FOLDER}/GenomeDir/ \
+	--readFilesIn \
+    data/processed/${END}/${SRR}_1.fastq.gz \
+    data/processed/${END}/${SRR}_2.fastq.gz \
+	--outFileNamePrefix results/star/${END}/${SRR}/${SRR}- \
+	--readFilesCommand zcat
