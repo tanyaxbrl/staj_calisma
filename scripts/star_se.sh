564d96f87ca1915309a1986ad7e3585c76bf29a3
nursenakocaturk <nursenakocaturk26@gmail.com>
2023-03-15 22:53:19 +0300

    hata düzeltildi

--- scripts/star_se.sh
+++ scripts/star_se.sh
@@ -13,6 +13,6 @@ REF_FOLDER=data/ref/
 STAR --runThreadN ${THREADS} \
 	--genomeDir ${REF_FOLDER}/GenomeDir/ \
 	--readFilesIn \
-    data/processed/${END}/${SRR}_1.fastq.gz \
+    data/processed/${END}/${SRR}.fastq.gz \
 	--outFileNamePrefix results/star/${END}/${SRR} \
 	--readFilesCommand zcat
