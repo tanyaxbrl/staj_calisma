7de47be719cfa84966be8331f6bc573707c4aca7
Nursena Kocatürk <95756231+nursenakocaturk@users.noreply.github.com>
2023-05-05 17:29:14 +0300

    duzenlemeler yapildi

--- rnaseq.R
+++ rnaseq.R
@@ -1,10 +1,11 @@
 library("Rsamtools")
-filenames <- dir("results/star/", pattern = "sorted.bam", full.names = T, recursive = T)
 
+filenames <- dir("results/star/", pattern = "sorted.bam", full.names = T, recursive = T)
 
 bamfiles <- BamFileList(filenames, yieldSize=2000000)
 
 library("GenomicFeatures")
+
 gtffile <- dir(".", pattern = "gtf", full.names = T)
 
 txdb <-  makeTxDbFromGFF("https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/412/675/GCF_000412675.1_ASM41267v1/GCF_000412675.1_ASM41267v1_genomic.gff.gz", format="gff")
@@ -12,6 +13,7 @@ txdb <-  makeTxDbFromGFF("https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/412/6
 ebg <- exonsBy(txdb, by="gene")
 
 library("GenomicAlignments")
+
 library("BiocParallel")
 
 register(MulticoreParam(2))
@@ -22,5 +24,7 @@ se <- summarizeOverlaps(features=ebg,
                         singleEnd=FALSE,
                         ignore.strand=TRUE,
                         fragments=TRUE )
+                        
+se <- as.data.frame(se)
 
-write.table(x = count, file = "counts.txt", col.names = T, row.names = T, quote = F)
+write.table(x = se, file = "counts.txt", col.names = T, row.names = T, quote = F)
