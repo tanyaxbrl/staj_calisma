a9f8d27265a7da6a6585d1afd22e0a569ea4b546
emrahkirdok <emrahkirdok@gmail.com>
2023-08-11 10:24:38 +0300

    sra-tools artik yok, fastqc betikleri part1 icine eklendi

--- part1.sh
+++ part1.sh
@@ -5,15 +5,17 @@ do
 	SRR=$(echo "$LINE" | cut -d " " -f1)
 	END=$(echo "$LINE" | cut -d " " -f2)
 
-	./scripts/download_fastq.sh ${SRR} ${END}
+# 	sra-tools conda ile hata veriyor, o yuzden bu kismi yapmayalim artik
+#	./scripts/download_fastq.sh ${SRR} ${END}
+
 	echo $END
 	if [[ ${END} == "pe" ]]
 	then
 		echo "pe"
-		./scripts/fastqc_pe.sh ${SRR} ${END}
+		fastqc data/raw/${END}/${SRR}_1.fastq data/raw/${END}/${SRR}_2.fastq
 	else
 		echo "se"
-		./scripts/fastqc_se.sh ${SRR} ${END}
+		fastqc data/raw/${END}/${SRR}.fastq
 	fi
 
 done < data.txt 
--- scripts/fastqc_se.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SRR=${1}
-END=${2}
-
-fastqc data/raw/${END}/${SRR}.fastq
-
-
