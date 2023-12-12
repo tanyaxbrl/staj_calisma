221e94174898eb03d62e57852ef7ba74bc5b8371
Tanya Beril Korkmaz <tanyaxbrl@gmail.com>
2023-12-12 17:44:46 +0300

    scripts/bowtie_se.sh

--- scripts/bowtie_se.sh
+++ scripts/bowtie_se.sh
@@ -7,9 +7,6 @@ FASTA=$3
 
 OUT=results/alignment/bowtie2
 
-RAW_OUT=data/raw
-REF_FOLDER=data/ref/
-
 echo "aligning ${SRR}"
 
 bowtie2-align-s -x data/ref/${FASTA} -p 4 -U results/processed/${END}/${SRR}.fastq.gz |  samtools view -F3 -q30 -Sb > ${OUT}/${SRR}.bam
