ebb6576a6c0acef92f095a8e162aab461a6c092a
Tanya Beril Korkmaz <tanyaxbrl@gmail.com>
2023-12-12 18:31:23 +0300

    r environment and part-count script fixed

--- part-count.sh
+++ part-count.sh
@@ -1,9 +1,6 @@
 
 
-TOOLS="bwa bowtie2 star"
+TOOL=$1
+ 
+featureCounts -T4 -t CDS -a data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -o results/counts/counts-${TOOL}.txt results/alignment/${TOOL}/*.sorted.bam
 
-for TOOL in ${TOOLS}
-do
-	featureCounts -T4 -t CDS -a data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -o results/counts/counts-${TOOL}.txt results/alignment/${TOOL}/*.sorted.bam
-
-done
