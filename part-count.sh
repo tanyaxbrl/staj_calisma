3eceb70487fafe894eddc377466aef027470b607
Tanya Beril Korkmaz <tanyaxbrl@gmail.com>
2023-12-14 18:53:11 +0300

    feature count pe vs se specified

--- part-count.sh
+++ part-count.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
 
+TOOL=$2
 
-TOOL=$1
- 
-featureCounts -T4 -t CDS -a data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -o results/counts/counts-${TOOL}.txt results/alignment/${TOOL}/*.sorted.bam
+while read LINE
+do
+        SRR=$(echo "$LINE" | cut -d " " -f1)
+        END=$(echo "$LINE" | cut -d " " -f2)
 
+        if [ ${END} == "pe" ]
+        then
+		featureCounts -p -T4 -t CDS -a data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -o results/counts/counts-${SRR}-${TOOL}.txt results/alignment/${TOOL}/${SRR}.sorted.bam
+        else
+		featureCounts -T4 -t CDS -a data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -o results/counts/counts-${SRR}-${TOOL}.txt results/alignment/${TOOL}/${SRR}.sorted.bam
+        fi
+
+done < $1
