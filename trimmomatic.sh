9fa55c95203834b50f1a0c9e2fcc851aa459cf43
Tanya Beril Korkmaz <tanyaxbrl@gmail.com>
2023-12-26 02:46:02 +0300

    trimmomatic script added

---
+++ trimmomatic.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir -p results/trimmed/se
+mkdir -p results/trimmed/pe
+
+
+while read LINE
+do 
+
+SRR = $(echo "$LINE" | cut -d " " -f1)
+END = $(echo "$LINE" | cut -d " " -f2)
+
+if [[${END} == "pe"]]
+then
+echo "pe"
+./scripts/trimmomatic_pe.sh ${SRR} ${END}
+
+else
+
+echo "se"
+echo ${SRR}
+echo ${END}
+./scripts/trimmomatic_se.sh ${SRR} ${END}
+
+fi
+
+done<$1
