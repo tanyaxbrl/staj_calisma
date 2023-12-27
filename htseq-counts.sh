e01af551eb004b17e65cec25ba6ec8f772f757c0
Tanya Beril Korkmaz <tanyaxbrl@gmail.com>
2023-12-27 17:02:43 +0300

    htseq script fixed

--- htseq-counts.sh
+++ htseq-counts.sh
@@ -10,10 +10,10 @@ do
 
 	if [ ${END} == "pe" ]
 	then
-		htseq-count results/alignment/${TOOL}/${SRR}.sorted.bam data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -r -t exon -c results/counts/counts-${SRR}-${TOOL}_pe.txt
+		htseq-count results/alignment/${TOOL}/${SRR}.sorted.bam data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -t exon > results/counts/counts-${SRR}-${TOOL}.txt
 	#phyton -m HTSeq.scripts.count -r -c results/counts/counts-${SRR}-${TOOL}_pe.txt results/alignment/bowtie2/${TOOL}/${SRR}.bam data/ref/GCA_000007565.2_ASM756v2_genomic.gtf
 	else
-		htseq-count results/alignment/${TOOL}/${SRR}.sorted.bam data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -t exon -c results/counts/counts-${SRR}-${TOOL}_se.txt
+		htseq-count results/alignment/${TOOL}/${SRR}.sorted.bam data/ref/GCA_000007565.2_ASM756v2_genomic.gtf -t exon > results/counts/counts-${SRR}-${TOOL}.txt
 	#phyton -m HTSeq.scripts.count -c results/counts/counts-${SRR}-${TOOL}_se.txt results/alignment/bowtie2/${TOOL}/${SRR}.bam data/ref/GCA_000007565.2_ASM756v2_genomic.gtf
 fi
 
